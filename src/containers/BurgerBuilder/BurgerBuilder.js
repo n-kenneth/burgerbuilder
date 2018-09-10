ad3c4a39a790064250f4b74ecbe4f467f2b60d61
Niel Kenneth <nkenneth19@gmail.com>
2018-09-10 20:53:34 +0800

    First commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Auxs from '../../hoc/Auxs';
+
+class BurgerBuilder extends Component {
+  render() { 
+    return (
+      <Auxs>
+        <div>Burger</div>
+        <div>Built Controls</div>
+      </Auxs>
+    );
+  }
+}
+ 
+export default BurgerBuilder;
